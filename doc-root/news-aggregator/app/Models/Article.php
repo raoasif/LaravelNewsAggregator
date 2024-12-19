6545682a822152deb2ca1f2a8cf00257c3ca4030
raoasif <raomuhammadasif91@gmail.com>
2024-12-19 23:41:53 +0500

    added fillable to articles

--- doc-root/news-aggregator/app/Models/Article.php
+++ doc-root/news-aggregator/app/Models/Article.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = [
+        'source_api_id',
+        'source_name',
+        'title',
+        'author', 
+        'description',
+        'content',
+        'url',
+        'url_to_image',
+        'category',
+        'section',
+        'published_at'
+    ];
 }
